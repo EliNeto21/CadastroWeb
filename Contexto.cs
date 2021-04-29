aebaf1f6fe3e0bf0e58f99b2d933ab8388770eee
Eli Neto <elineto91@gmail.com>
2021-04-28 23:08:33 -0300

    Adicionar arquivos de projeto.

---
+++ Contexto.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Criação do contexto para criar a base e tebala se não existirem
+/// </summary>
+
+namespace CdastroWeb.Models
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Usuario> Usuario { get; set; }
+
+    }
+}
---
+++ Models/Usuarios.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+/// <summary>
+/// Criação de todas as Models pertencentes aos usuários
+/// </summary>
+
+namespace CdastroWeb.Models
+{
+    [Table("Usuario")]
+    public class Usuario
+    {
+        [Display(Name = "Id")]
+        [Column("Id")]
+        public int Id { get; set; }
+
+        [Display(Name = "Nome")]
+        [Column("Nome")]
+        public string Nome { get; set; }
+
+        [Display(Name = "Celular")]
+        [Column("Celular")]
+        public string Celular { get; set; }
+
+        [Display(Name = "Email")]
+        [Column("Email")]
+        public string Email { get; set; }
+    }
+}
